f2f3af1141f3b54f8302078f24da9e49d65a6f8a
disiqueira <diego@astrolink.com.br>
2017-01-21 12:02:47 -0200

    New README file and example

--- example_test.go
+++ example_test.go
@@ -6,7 +6,26 @@ import (
 
 func ExamplePrintTree() {
 
-	obj := gotree.ReadFolder("/Users/disiqueira/Documents/Arduino/")
+	obj := gotree.ReadFolder("/Users/disiqueira/Documents/placa_display_led")
 	gotree.PrintTree(obj)
+}
 
+func ExamplePrintCustom() {
+
+	var artist gotree.GTStructure
+	artist.Name = "Pantera"
+
+	var album gotree.GTStructure
+	album.Name = "Far Beyond Driven"
+
+	var music gotree.GTStructure
+	music.Name = "5 Minutes Alone"
+
+	//Add Music to the Album
+	album.Items = append(album.Items, music)
+
+	//Add Album to the Artist
+	artist.Items = append(artist.Items, album)
+
+	gotree.PrintTree(artist)
 }
